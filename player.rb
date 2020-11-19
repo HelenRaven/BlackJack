62f8133e233646ec24957886cf90201ee94d085f
Elena Graivoronskaya <helenraven@mail.ru>
2020-11-19 16:18:34 +0700

    scores, make bet, take win methods added to Player class

--- player.rb
+++ player.rb
@@ -1,13 +1,33 @@
 class Player
 
-  attr_accessor :bank, :cards
-  attr_reader :name
-
+  attr_reader :name, :score, :bank, :cards
 
   def initialize(name, bank)
     @name = name
     @cards = []
     @bank = bank
+    @score = 0
+  end
+
+  def scores
+    aces = 0
+    @cards.each do |card|
+      aces += 1 if card[value] == :T
+      @score += card[cost]
+    end
+    aces.times { @score -= 10 if @score > 21 }
+  end
+
+  def make_bet(value)
+    @bank -= value
+  end
+
+  def get_win(value)
+    @bank += value
+  end
+
+  def take_card(card)
+    @cards << card
   end
 
 end
