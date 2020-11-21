1eddf218739729e03d5ed5d97d70b4ce540be9d3
Elena Graivoronskaya <helenraven@mail.ru>
2020-11-21 12:18:42 +0700

    Player is inheritor of Dealer

--- player.rb
+++ player.rb
@@ -1,33 +1,22 @@
-class Player
+require_relative 'dealer'
 
-  attr_reader :name, :score, :bank, :cards
+class Player < Dealer
 
-  def initialize(name, bank)
+  attr_accessor :move
+
+  MOVE = [{key: :G, text: "Get card"}, {key: :S, text: "Skip move"}, {key: :O, text: "Open cards"}]
+
+  def initialize (name, bank)
     @name = name
-    @cards = []
-    @bank = bank
-    @score = 0
+    @move = []
+    @move += MOVE
+    super
   end
 
-  def scores
-    aces = 0
-    @cards.each do |card|
-      aces += 1 if card[value] == :T
-      @score += card[cost]
-    end
-    aces.times { @score -= 10 if @score > 21 }
-  end
-
-  def make_bet(value)
-    @bank -= value
-  end
-
-  def get_win(value)
-    @bank += value
-  end
-
-  def take_card(card)
-    @cards << card
+  def reset
+    @move.clear
+    @move += MOVE
+    super
   end
 
 end
